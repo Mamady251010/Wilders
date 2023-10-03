b1038c512c1f16ee75a6bcc75cf761fbb9af9cd6
Mamady251010 <mamady44.camara@gmail.com>
2023-10-03 10:58:57 +0200

    changement

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+import chalk from "chalk"
+
+console.log(
+    chalk.blue( "Ange")
+)
+console.log(
+    chalk.blue( "Vincent")
+)
+console.log(
+    chalk.blue( "Karime")
+)
+console.log(
+    chalk.blue( "Alexendra")
+)
+
